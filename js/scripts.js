af8d1c4051c39d0a061eb3ca64916fd64eef2aaa
igl-myrick <igl.myrick@gmail.com>
2023-11-07 15:54:44 -0800

    clear previous form submissions upon resubmission

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
-  let createVar = document.querySelector("select#create").value;
-  let internetVar = document.querySelector("select#internet").value;
-  let efficiency = document.querySelector("select#efficiency").value;
-  let gameVar = document.querySelector("select#games").value;
 
   form.addEventListener("submit", function(e) {
     e.preventDefault();
+
+    document.getElementById("result4").setAttribute("class", "hidden");
+    document.getElementById("result3").setAttribute("class", "hidden");
+    document.getElementById("result2").setAttribute("class", "hidden");
+    document.getElementById("result1").setAttribute("class", "hidden");
+
+    let createVar = document.querySelector("select#create").value;
+    let internetVar = document.querySelector("select#internet").value;
+    let efficiency = document.querySelector("select#efficiency").value;
+    let gameVar = document.querySelector("select#games").value;
+
     if (createVar === "I like the idea of building onto something") {
       return document.getElementById("result4").removeAttribute("class");
     } else if (internetVar === "Yes") {

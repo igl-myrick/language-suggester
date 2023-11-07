3a6ea975bab29396767f33e0454935ecf59efa98
igl-myrick <igl.myrick@gmail.com>
2023-11-07 15:35:11 -0800

    add initial branching code to display results

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
-// business logic
-
-// ui logic
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
+  let createVar = document.querySelector("select#create").value;
+  let internetVar = document.querySelector("select#internet").value;
+  let efficiency = document.querySelector("select#efficiency").value;
+  let gameVar = document.querySelector("select#games").value;
 
   form.addEventListener("submit", function(e) {
     e.preventDefault();
-    document.querySelector("div#result1").removeAttribute("class");
+    if (createVar === "I like the idea of building onto something") {
+      return document.getElementById("result4").removeAttribute("class");
+    } else if (internetVar === "Yes") {
+      return document.getElementById("result3").removeAttribute("class");
+    } else if ((efficiency === "Yes") || (gameVar === "No")) {
+      return document.getElementById("result2").removeAttribute("class");
+    } else {
+      return document.getElementById("result1").removeAttribute("class");
+    }
   });
 });

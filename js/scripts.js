0f6cd7cad5b7b6b52d74c7729aeaeedfb432dce5
igl-myrick <igl.myrick@gmail.com>
2023-11-09 14:50:45 -0800

    reword variables to be more consistent

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,14 @@ window.addEventListener("load", function() {
 
     let createVar = document.querySelector("select#create").value;
     let internetVar = document.querySelector("select#internet").value;
-    let efficiency = document.querySelector("select#efficiency").value;
+    let organizedVar = document.querySelector("select#efficiency").value;
     let gameVar = document.querySelector("select#games").value;
 
     if (createVar === "I like the idea of building onto something") {
       return document.getElementById("result4").removeAttribute("class");
     } else if (internetVar === "Yes") {
       return document.getElementById("result3").removeAttribute("class");
-    } else if ((efficiency === "Yes") || (gameVar === "No")) {
+    } else if ((organizedVar === "Yes") || (gameVar === "No")) {
       return document.getElementById("result2").removeAttribute("class");
     } else {
       return document.getElementById("result1").removeAttribute("class");
